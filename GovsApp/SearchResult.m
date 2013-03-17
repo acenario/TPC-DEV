506d3846741150dc7d19ad6517411a14ce84926c
Arjun Bhatnagar <me@arjunb.com>
2013-03-16 23:13:12 -0400

    Deleted DS Store
    DS Store deletion

---
+++ GovsApp/SearchResult.m
@@ -0,0 +1,20 @@
+//
+//  SearchResult.m
+//  GovsApp
+//
+//  Created by Arjun Bhatnagar on 3/16/13.
+//  Copyright (c) 2013 The Productions Club. All rights reserved.
+//
+
+#import "SearchResult.h"
+
+@implementation SearchResult
+
+@synthesize name = _name;
+@synthesize place = _place;
+@synthesize time = _time;
+
+
+
+
+@end
